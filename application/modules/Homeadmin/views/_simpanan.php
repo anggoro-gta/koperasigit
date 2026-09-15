ea61460cd45713e981696bd5c9bbc9e8eb68b16f
developer-bappeda <gigihtrianung@gmail.com>
2026-09-15 14:07:28 +0700

    gta

--- application/modules/Homeadmin/views/_simpanan.php
+++ application/modules/Homeadmin/views/_simpanan.php
@@ -324,7 +324,7 @@
 <div class="col-md-6 col-sm-12 col-xs-12">
     <div class="x_panel tile">
         <div class="x_title">
-            <h2>Detail Laba</h2>
+            <h2>Detail Selisih Simpanan</h2>
             <div class="clearfix"></div>
         </div>
         <div class="x_content">
@@ -433,7 +433,7 @@
 <div class="col-md-6 col-sm-12 col-xs-12">
     <div class="x_panel">
         <div class="x_title">
-            <h2>Grafik Laba</h2>
+            <h2>Grafik Selisih Simpanan</h2>
             <div class="clearfix"></div>
         </div>
         <div class="x_content2">
